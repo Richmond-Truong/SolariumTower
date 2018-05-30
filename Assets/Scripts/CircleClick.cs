9f01a6802ba23fb39b59c3aeeac661c2c8177dd2
Richmond-Truong <Richmond.t@hotmail.ca>
2018-05-29 21:31:31 -0400

    added cs

---
+++ Assets/Scripts/CircleClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CircleClick : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast (ray, out hit, 100.0f)){
+				Debug.Log("test2");
+			}
+		}
+	}
+}
